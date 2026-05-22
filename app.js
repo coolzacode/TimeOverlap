fe57511af1e404dea4927285422e92334e5d25db
hazzaah <154761475+hazzaah@users.noreply.github.com>
2026-05-21 20:13:47 -0500

    refactor: use INTL in MemberProfile class

--- app.js
+++ app.js
@@ -6,12 +6,55 @@ const memberForm = document.getElementById('member-form');
 let membersArr = []
 
 class MemberProfile {
-    constructor (name, location, timezoneOffset, startTime, endTime) {
+    constructor (name, location, timezone, localStart, localEnd) {
         this.name = name;
         this.location = location;
-        this.timezoneOffset = Number(timezoneOffset);
-        this.startTime = Number(startTime);
-        this.endTime = Number(endTime);
+        this.timezoneOffset = timezone;
+        this.localStart = Number(localStart);
+        this.localEnd = Number(localEnd);
+    }
+
+    // get's the current GMT offset as a float (ex. -5 or 5.5)
+    getUTCOffset() {
+        const offsetFormatter = new Intl.DateTimeFormat('en-US', {
+            timeZone: this.timezone,
+            timeZoneName: 'shortOffset'
+        });
+
+        const dateTimeParts = offsetFormatter.formatToParts(new Date());
+        const timezonePart = dateTimeParts.find(part => part.type === 'timeZoneName');
+        const rawOffsetString = timezonePart.value;
+
+        const numericOffsetString = rawOffsetString.replace('GMT', '').replace(':', '.');
+        const decimalOffset = parseFloat(numericOffsetString);
+
+        return decimalOffset || 0;
+    }
+
+    // gets the abbreviation of the timezone
+    getTimezoneAbbr() {
+        const abbrFormatter = new Intl.DateTimeFormat('en-US', {
+            timeZone: this.timezone,
+            timeZoneName: 'short'
+        });
+
+        const dateTimeParts = abbrFormatter.formatToParts(new Date());
+        const timezonePart = dateTimeParts.find(part => part.type === 'timeZoneName');
+
+        return timezonePart.value;
+    }
+
+    // converts local hours to UTC grid indices (0-23)
+    getUTCWorkHours() {
+        const currentOffset = this.getUTCOffset(); 
+
+        const hourIndexStartUTC = (this.localStart - currentOffset + 24) % 24;
+        const hourIndexEndUTC = (this.localEnd - currentOffset + 24) % 24;
+
+        return {
+            startUTC: hourIndexStartUTC, 
+            endUTC: hourIndexEndUTC
+        };
     }
 }
 
